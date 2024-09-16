996ad83f31e8ecb9cace093b4e043b7810261d3a
deepsikhacode <deepshikhap350@gmail.com>
2024-09-16 13:36:43 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full mb-10 pt-7 p-6 sm:px-20 flex justify-between items-center">
+      <h1 className="w-28 text-3xl font-bold">Summify</h1>
+      
+    </nav>
+  );
+};
+
+export default Navbar;
